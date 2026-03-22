424ed56d4c006751edc2c645fb112d3c7c942e4c
Akshar Munjani <aksharmunjani127@gmail.com>
2026-03-22 14:35:11 +0530

    feat: Integrate Vercel Analytics and Speed Insights into the root layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
     title: "Akshar Portfolio",
@@ -14,7 +15,11 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body>{children}<Analytics /></body>
+            <body>
+                <Analytics />
+                <SpeedInsights />
+                {children}
+            </body>
         </html>
     );
 }
